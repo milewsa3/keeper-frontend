5c956f83086e4ec55a79fe8b4226c7f4dc3f7933
Adrian <a.milewski@onet.eu>
2022-01-24 20:03:20 +0100

    Usuniecie authMiddleware

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -3,7 +3,6 @@ import authReducer from './auth/authSlice';
 import themeReducer from './theme/themeSlice';
 import passwordEntityReducer from './passwordEntity/passwordEntitySlice';
 import { loadSessionState, saveSessionState } from './sessionStorage';
-import axiosAuth from '../auth/authMiddleware';
 
 const persistedState = loadSessionState()
 export const store = configureStore({
@@ -12,7 +11,6 @@ export const store = configureStore({
     theme: themeReducer,
     passwordEntity: passwordEntityReducer
   },
-  middleware: [axiosAuth],
   preloadedState: persistedState
 });
 
